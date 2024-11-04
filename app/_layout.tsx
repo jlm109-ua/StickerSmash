7a60a9de4d6440ab1cb8eede62662a425a9a7829
jimy <jlm109@alu.ua.es>
2024-11-04 10:36:03 +0100

    "Configure status bar, splash screen and app icon" finished

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,15 @@
 import { Stack } from "expo-router";
 import { StyleSheet } from 'react-native';
+import { setStatusBarStyle } from 'expo-status-bar';
+import { useEffect } from 'react';
 
 export default function RootLayout() {
+  useEffect(() => {
+    setTimeout(() => {
+      setStatusBarStyle('light');
+    }, 0);
+  }, []);
+
   return (
     <Stack
       screenOptions={{
